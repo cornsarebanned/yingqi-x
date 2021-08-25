4a3ce6d08af3be8c3265effb153ea2cff5a13b57
cornsarebanned <yqseah001@gmail.com>
2021-08-25 23:47:47 +0800

    cache clean reinstall node_modules

--- pages/index.js
+++ pages/index.js
@@ -14,49 +14,9 @@ export default function Home() {
         <meta name="keywords" content="Portfolio" />
       </Head>
 
-      {/* <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 0.4,
-            },
-          },
-        }}
-      >
-        <h3 className="home-title-name">__________________</h3>
-      </motion.div> */}
-
       <div className="home-title-job-container">
         <h1 className="home-title-job">UX | <span className="title-background">UI</span></h1>  
       </div>
-
-      {/* <div className="projects-wrapper">
-
-        <div className="card">
-          <div className="card-content"> */}
-            {/* <grid-item><img className="grid-image" src="/logo_light.png" width="100%" height="auto" alt="ezbuy" /></grid-item> */}
-            {/* <h2 className="card-title">
-              EZBUY|<span className="title-nobold">UX Case Study</span>
-            </h2>
-            <p className="card-body">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text.
-            </p>
-            <Link href="#">
-              <a className="card-btn">Learn More</a>
-            </Link>
-          </div>
-        </div>
-        
-      </div> */}
     </>
   );
 }
